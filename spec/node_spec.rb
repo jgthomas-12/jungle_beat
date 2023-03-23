46426e7a8487b5071de77d18338a537e6c154f9f
Jesse Thomas <jgthomas3@gmail.com>
2023-03-23 17:04:33 -0600

    Establish 'exists' test in node_spec.rb

---
+++ spec/node_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require "./lib/node"
+
+RSpec.describe Node do
+  it "exists" do
+    node = Node.new("plop")
+    
+    expect(node.data).to eq("plop")
+    expect(node.next_node).to eq(nil)
+  end
+end
