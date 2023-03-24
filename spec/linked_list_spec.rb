d74edee73f095b8d5254a6fbe24a806224383f3b
Jesse Thomas <jgthomas3@gmail.com>
2023-03-24 15:22:41 -0600

    Add append and counter methods with notes, start to_string method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -4,28 +4,38 @@ require "./lib/node"
 
 RSpec.describe LinkedList do
   it "exists" do
-
     list = LinkedList.new
+
     expect(list).to be_an_instance_of(LinkedList)
     
   end
 
   it "appends" do
     list = LinkedList.new
-    # expect(list).to be_an_instance_of(LinkedList)
-    
+  
     expect(list.head).to be(nil)
-    #   #=> nil
     expect(list.append("doop")).to eq("doop")
-    #   #=> "doop"
   end
 
-    # expect(list).to eq()
-    #   #=> <LinkedList head=<Node data="doop" next_node=nil #5678904567890> #45678904567>
-    # list.head.next_node
-    #   #=> nil
-    # list.count
+  it "has accessible data" do
+    list = LinkedList.new
+
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+    expect(list.head.next_node).to eq(nil)
+  end
+
+  it "counts" do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.count).to eq(1)
     #   #=> 1
-    # list.to_string
+  end
+
+  it "can become a string" do
+    list = LinkedList.new
+    # require 'pry'; binding.pry
+    expect(list.to_string).to eq("doop")
     #   #=> "doop"
+  end
 end
