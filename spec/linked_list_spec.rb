8a2eeeed8bc3c214a273d740f32f705f7cf95679
Jesse Thomas <jgthomas3@gmail.com>
2023-03-25 13:51:01 -0600

    Add prepend and insert tests in linked_list_spec.rb

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe LinkedList do
     list = LinkedList.new
 
     expect(list).to be_an_instance_of(LinkedList)
-    
   end
+
   it "has a head attribute" do
     list = LinkedList.new
 
@@ -36,12 +36,47 @@ RSpec.describe LinkedList do
     expect(list.count).to eq(2)
   end
 
-  xit "can become a string" do
+  it "can become a string" do
     list = LinkedList.new
     list.append("doop")
     list.append("deep")
 
     expect(list.to_string).to eq("doop deep")
+  end
+
+  it "can prepend" do
+    list = LinkedList.new
+    list.append("plop")
+    
+    expect(list.head.data).to eq("plop")
+    expect(list.head.next_node).to eq(nil)
+    expect(list.to_string).to eq("plop")
+    
+    list.append("suu")
+
+    expect(list.head.next_node.data).to eq("suu")
+    
+    list.prepend("dop")
+    
+    expect(list.head.data).to eq("dop")
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+  end 
+
+  xit "can insert" do
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+
+    expect(list.head.data).to eq("plop")
+    expect(list.head.next_node).to eq(nil)
+    expect(list.to_string).to eq("plop")
+    expect(list.head.next_node.data).to eq("suu")
+    
+    list.insert(1, "woo")
+    expect(list.head.next_node.data).to eq("woo")
+    expect(list.to_string).to eq("dop woo plop suu")
+    
   end
 end
 
