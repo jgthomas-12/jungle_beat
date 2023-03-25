5d804ac5808307a188811251e13c1b28a1dc872b
Jesse Thomas <jgthomas3@gmail.com>
2023-03-24 20:14:05 -0600

    Clean up node_spec.rb file

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Node do
     node = Node.new("plop")
     
     expect(node).to be_an_instance_of(Node)
+  end
+  
+  it "has attributes" do
+    node = Node.new("plop")
+    
     expect(node.data).to eq("plop")
     expect(node.next_node).to eq(nil)
   end
