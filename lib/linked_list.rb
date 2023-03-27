d8c0e0c00c24d172300a68cbafa990d9fdf3e3e9
Jesse Thomas <jgthomas3@gmail.com>
2023-03-26 18:46:29 -0600

    Add find and insert method (2 versions) and clean up comments in linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -41,74 +41,61 @@ class LinkedList
     end
   end
 
-  def insert(index, data)
+  def find(index, data)
     current_node = @head
-    (index - 1).times do #finds the position just before index
-      current_node = current_node.next_node #current node next_node lines up for insertion
+    
+    if data > 1
+      sound_list = []
+      data.times do
+        current_node = current_node.next_node #current node next_node lines up for insertion
+        sound_list << current_node.data
+      end  
+      return sound_list.join(" ")
+    else
+      (index - 1).times do #finds the position just before index
+        current_node = current_node.next_node #current node next_node lines up for insertion
+      end
     end
-    insert_node = Node.new(data)
-    insert_node.next_node = current_node.next_node #attaches
-    current_node.next_node = insert_node
+    current_node.next_node.data
   end
-  
 
-  # def insert(new_position_value, data)
-  #   # current_node = @head
-  #   index_counter = 0 
-  #     if @head != nil
-  #       place_holder = nil
-  #       index_counter = 1
-  #       insert_node = Node.new(data)
-  #       while index_counter < new_position_value
-  #         current_node = current_node.next_node
-  #         index_counter += 1
-  #         if index_counter == new_position_value
-  #           place_holder = insert_node.next_node #this should make a space in insert_node.next_node
-  #           insert_node.next_node = insert_node   #this should actually insert the insert_node
-  #           insert_node.next_node = place_holder #this should refer to the new insert_node's next_node and add the held value with other values attached to it     
-  #         end
-  #         break
-  #       end 
-  #     else @head == nil
-  #       @head = Node.new(data)
-  #     end
-  #   # need to make a new node to be inserted 
-  #   # if empty, make it the head. 
-  #   # if you're trying to insert more than is there, throw error
-  #   # example: insert in index 4 but there are only 2 linked list items = "error, list not long enough"
-  #   # The inserted node should become the next_node of the node prior and will have the node after become the next_node of current_node
-  # end
-
-  # def insert(new_position_value, data)
-  #   # current_node = Node.new(data)
-  #   # placeholder = nil
-  #   index_counter = 0 
-  #   if head != nil
-  #     place_holder = nil
-  #     index_counter = 1
-  #     insert_node = Node.new(data)
-  #     while index_counter < new_position_value
-  #       insert_node = insert_node.next_node
-  #       index_counter += 1
-  #       # proceed to next node
-  #       if index_counter == new_position_value
-  #         place_holder = insert_node.next_node #this should make a space in insert_node.next_node
-  #         insert_node.next_node = insert_node   #this should actually insert the insert_node
-  #         insert_node.next_node = place_holder #this should refer to the new insert_node's next_node and add the held value with other values attached to it     
-  #       end
-  #       break
-  #     end
-  #   else #@head == nil
-  #     # require 'pry'; binding.pry
-  #     @head = Node.new(data)
-  #     # place_holder = nil
+  # def insert(index, data)
+  #   current_node = @head
+  #   (index - 1).times do #finds the position just before index
+  #     current_node = current_node.next_node #current node next_node lines up for insertion
   #   end
-  #   # need to make a new node to be inserted 
-  #   # if empty, make it the head. 
-  #   # if you're trying to insert more than is there, throw error
-  #   # example: insert in index 4 but there are only 2 linked list items = "error, list not long enough"
-  #   # The inserted node should become the next_node of the node prior and will have the node after become the next_node of current_node
+  #   insert_node = Node.new(data)
+  #   insert_node.next_node = current_node.next_node #attaches
+  #   current_node.next_node = insert_node
   # end
+
+  # this method passes but...
+  # if it passes index 0 what happens...index -1.times would break it?
+  # this also can't insert a head into the list if nothing exists 
+
+  def insert(new_position_value, data)
+    current_node = @head
+    index_counter = 0 
+      if @head != nil
+        place_holder = nil
+        index_counter = 1
+        insert_node = Node.new(data)
+        while index_counter < new_position_value
+          current_node = current_node.next_node
+          index_counter += 1
+        end
+        place_holder = current_node.next_node #this should make a space in insert_node.next_node
+        current_node.next_node = insert_node   #this should actually insert the insert_node
+        insert_node.next_node = place_holder #this should refer to the new insert_node's next_node and add the held value with other values attached to it    
+      else @head == nil
+        @head = Node.new(data)
+      end
+    # need to make a new node to be inserted 
+    # if empty, make it the head. 
+    # if you're trying to insert more than is there, throw error
+    # example: insert in index 4 but there are only 2 linked list items = "error, list not long enough"
+    # The inserted node should become the next_node of the node prior and will have the node after become the next_node of current_node
+  end
   
   def count
     current_node = @head
@@ -123,17 +110,6 @@ class LinkedList
     end
     counter
   end
-  
-  # def to_string
-  #   if count <= 1
-  #       @head.data
-  #     else
-  #       @head.data + " " + find_tail.next_node.data
-  #       require 'pry'; binding.pry
-  #     end
-  #     # Need to loop this action -> + " " + find_tail.next_node.data
-  #   # @head.data
-  # end
   
   def to_string
     beats = []
@@ -157,24 +133,10 @@ class LinkedList
       end
       current_node
     end
-    #   while node = node.next_node
-    #     if node.next_node == false
-    #       node
-    #     end
-    #   end
-    # return node if !node.next_node
-    # return node if !node.next_node
-    #   while (node = node.next)
-    # end
-    # #node variable is assigned the @head attribute
-    # #push to end of list while next_node is full
-    # while @head.next_node != nil
-    #   #go to next node
-    #   if @head.next_node == nil
-    #   end
-    # end
   end
 end
+
+
 #check next node is nil
 #while or until loop
 #@until something is true or while something is true, continue until false
