bddfc6b950931d1caf8c4bac57d50b398b1b37f7
Jesse Thomas <jgthomas3@gmail.com>
2023-03-27 15:30:51 -0600

    Establish exists, atrributes, append and count tests in jungle_beat_spec.rb

---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,39 @@
+ require "rspec"
+ require "./lib/jungle_beat"
+ require "./lib/linked_list"
+ require "./lib/node"
+
+RSpec.describe JungleBeat do
+
+  it "exists" do
+    jb = JungleBeat.new
+
+    expect(jb).to be_an_instance_of(JungleBeat)
+  end
+
+  it "has readable attributes" do
+    jb = JungleBeat.new
+
+    expect(jb.list.head).to eq(nil)
+  end
+
+  it "can append multiple pieces of data" do
+    jb = JungleBeat.new
+    jb.append("deep doo ditt")
+
+    expect(jb.append("deep doo ditt")).to eq("deep doo ditt")
+    expect(jb.list.head.data).to eq("deep")
+    expect(jb.list.head.next_node.data).to eq("doo")
+    expect(jb.append("woo hoo shu")).to eq("woo hoo shu")
+  end
+
+  it "can count" do
+    jb = JungleBeat.new
+    jb.append("deep doo ditt")
+    jb.append("woo hoo shu")
+    
+    expect(jb.count).to eq(6)
+  end
+
+end
+
