24b3d0b2a15b3b21dade0ec732be259ae09194d1
Jesse Thomas <jgthomas3@gmail.com>
2023-03-28 16:31:39 -0600

    Be the final file node_spec.rb

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,17 +1,14 @@
 require "rspec"
 require "./lib/node"
 
-RSpec.describe Node do
-  
+RSpec.describe Node do 
   it "exists" do
     node = Node.new("plop")
-    
     expect(node).to be_an_instance_of(Node)
   end
   
   it "has attributes" do
     node = Node.new("plop")
-    
     expect(node.data).to eq("plop")
     expect(node.next_node).to eq(nil)
   end
