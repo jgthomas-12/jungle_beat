add90ce928025cc6bf3338bfdf18adacee06ab2c
Jesse Thomas <jgthomas3@gmail.com>
2023-03-28 16:32:49 -0600

    Be the final file node_spec.rb

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,5 @@
 class Node
   attr_reader :data, :next_node
-  # attr_accessor :next_node
   def initialize(data)
     @data = data
     @next_node = nil
@@ -13,6 +12,4 @@ class Node
   def data=(dat1)
     @data = dat1
   end
-  #establishes as a variable that can be edited...need to understand the setter method thing. Maybe it needs to be a different variable name to make it super obvious and unable to be mixed up?
-  
 end
