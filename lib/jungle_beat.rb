a07dfaac3085c44363ac072aaa345ef5f5aa7ea0
Jesse Thomas <jgthomas3@gmail.com>
2023-03-27 20:48:45 -0600

    Adjust initialize, add all method, add defined_beat? method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,14 +1,19 @@
 class JungleBeat
-
+  attr_accessor :data
   attr_reader :list
-  def initialize
-    @list = LinkedList.new
+  def initialize(data = " ")
+    @list = LinkedList.new #by calling this a new list is established with the attributes of the list
+    @data = append(data) #this works because I make a new node in the append method below
+  end
+
+  def all
+    list.to_string #to_string already lists the output in a string 
   end
 
   def append(data)
     datum = data.split(" ")
-    datum.each do |data|
-      list.append(data) 
+    datum.each do |data1|
+      list.append(data1) if defined_beats?(data1)
     end
     data
   end
@@ -19,7 +24,13 @@ class JungleBeat
 
   def play
     beats = list.to_string
-    `say -r 100 -v Boing #{beats}`
+    `say -r 175 -v Daniel #{beats}`
   end
 
+  def defined_beats?(data)
+    defined_beats = ["tee", "dee", "deep", "bop", "boop", "la", "na", "deep", "doo", "ditt", "woo", "hoo", "shu"]
+    defined_beats.include?(data)
+  end
 end
+
+# beats = ""
